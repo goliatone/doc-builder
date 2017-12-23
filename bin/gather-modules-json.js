807be43dd5f55cd5adc86540ee0452b0691b2b4c
goliatone <burgosemi@gmail.com>
2017-12-23 18:29:35 +0100

    add bin script

---
+++ bin/gather-modules-json.js
@@ -0,0 +1,38 @@
+
+function getModulesInfo(url){
+    return fetch(url).then((res)=>res.json());
+}
+
+/**
+ * someorjowej
+ * @param {Object} json Object
+ */
+function transformResults(json) {
+    let out = json.results.map((item)=>{
+        let mod = {};
+        mod.title = item.name[0];
+        mod.description = item.description[0];
+        mod.author = item.author[0];
+        mod.version = item.version[0];
+        mod.labels = (item.keywords || []).map((txt)=>{
+            return {
+                txt,
+                type: 'light-gray'
+            }
+        });
+
+        return mod;
+    });
+    
+    return Promise.resolve(out);
+}
+
+let uri = 'http://npmsearch.com/query?q=core.io&fields=name,author,readme,keywords,description,version,modified';
+
+(async ()=> {
+    let result = await getModulesInfo(uri);
+    result = await transformResults(result);
+    console.log(JSON.stringify(result, null, 4));
+})()
+
+
