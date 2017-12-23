9fceab6eea3b51aa86b481bc2b3aec5e5219012f
goliatone <burgosemi@gmail.com>
2017-12-23 18:54:59 +0100

    add rollup!

---
+++ rollup.config.js
@@ -0,0 +1,33 @@
+import svelte from 'rollup-plugin-svelte';
+import { babel, uglify } from 'rollup-plugin-bundleutils';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+
+let dev = process.env.NODE_ENV === 'development'; 
+
+export default {
+  input: 'output/js/app.js',
+  output: {
+    file: 'output/js/dist/main.js',
+    format: 'iife',
+  },
+  plugins: [
+    svelte({
+      // By default, all .html and .svelte files are compiled
+      extensions: ['.html'],
+
+      // You can restrict which files are compiled
+      // using `include` and `exclude`
+      include: 'output/js/components/**.html',
+    }),
+    babel({
+      include: ['./output/js/**/*.js'],
+    }),
+    uglify(),
+    dev ? serve({
+            contentBase: ['./output'],
+            open: true
+    }) : {},
+    dev ? livereload() : {}
+  ],
+};
