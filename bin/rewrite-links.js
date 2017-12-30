30613301ccfc8e1ead166cedc28ecde3123c79d8
goliatone <burgosemi@gmail.com>
2017-12-30 16:49:45 -0500

    Update rewrite-links, almost there

--- bin/rewrite-links.js
+++ bin/rewrite-links.js
@@ -1,9 +1,15 @@
 'use strict';
 
 const fx = require('../lib/fx');
-const {basename} = require('path');
+const { basename, resolve } = require('path');
 const cheerio = require('cheerio');
+const FileDescriptor = require('../lib/filedescriptor');
 const collectFiles = require('../lib/collect-files').collectFileDescriptors;
+
+const _prepend = function(str, char) {
+    if (str[0] !== char) return char + str;
+    return str;
+};
 
 //Elements that have url:
 //img -> src
@@ -18,148 +24,212 @@ const collectFiles = require('../lib/collect-files').collectFileDescriptors;
 
 const assets = ['img/', 'js/', 'css/'];
 
-const links = [{
-    description: 'Replace index.html for a /',
-    match:function(str){
-        return str.match(/.*(index.html)$/);
-    }, 
-    replace: function(src){
-        return src.replace('index.html', '/');
+const LINK_RULES = [
+    {
+        description: 'Replace index.html for a /',
+        match: function(str) {
+            return str.match(/.*(index.html)$/);
+        },
+        replace: function(src) {
+            return src.replace('index.html', '/');
+        }
+    },
+    {
+        description: 'Remove .html',
+        match: function(str) {
+            return !!str.match(/^(.*)\.html$/);
+        },
+        replace: function(src) {
+            return src.replace('.html', '');
+        }
+    },
+    {
+        description: 'Remove trailing slash',
+        match: function(str) {
+            if (str === '/') return false;
+            return str.match(/\/$/);
+        },
+        replace: function(src) {
+            return src.replace(/\/$/, '');
+        }
+    },
+    {
+        description: 'Make relative links absolute',
+        match: (x) => true,
+        replace: function(src) {
+            console.log('prepend', src);
+            return _prepend(src, '/');
+        }
     }
-}, {
-    description: 'Remove .html',
-    match: function(str) {
-        return !!str.match(/^(.*)\.html$/);
-    }, replace: function(src) {
-        return src.replace('.html', '');
-    }
-}, {
-    description: 'Remove trailing slash',
-    match: function(str) {
-        if(str === '/') return false;
-        return str.match(/\/$/);
-    }, 
-    replace: function(src){
-        return src.replace(/\/$/, '');
-    }
-}, {
-    description: 'Make relative links absolute',
-    match: isRelative,
-    replace: function(src) {
-        return '/' + src;
-    }
-}];
+];
 
-// const filename = './output/documentation/api/index.html';
-const filename = './output/documentation/api/index.html';
-fx.readFile(filename, 'utf-8').then( async (html) => {
+processDirectory('./output', process.cwd() + '/output');
 
-    const $ = cheerio.load(html);
+async function processDirectory(dirpath, cwd) {
+    let collection = await collectFiles('./output');
 
-    let files, collection;
+    let htmlFiles = collection.filterByExt('.html');
 
-    collection = await collectFiles('./output');
-
-    files = collection.filterByExt('.js');
-
-    // files.forEach((fd) => console.log('name', fd.basename, fd.relativePath));
+    let file = new FileDescriptor({source: './output/documentation/api/index.html', cwd});
     
-    //Ensure all scripts are pointing to the right thing:
+    try {
+        await processFile(file, collection);
+    } catch (error) {
+        console.log('error', error);
+    }
+
+    process.exit();
+
+    htmlFiles.forEachAsync(async (fd)=>{
+        fd.cwd = cwd;
+        try {
+            await processFile(fd, collection);
+        } catch (error) {
+            console.log('error', error);
+        }
+    });
+}
+
+//TODO: Collect all changes. from: {} to: {}
+//TODO: Collect all files referenced by a, img, script, link.
+//TODO: Assert that referenced files exist.
+//TODO: Show which files  where not referenced.
+async function processFile(indexFile, collection) {
+    try {
+        console.log('processing file ', indexFile.source, indexFile.name);
+        await indexFile.readFile();
+        console.log('... start html processor');
+        await processHtml(indexFile, collection);
+    } catch (error) {
+        console.log('processFile error!');
+        console.error(error);
+        throw error;
+    }
+}
+
+async function processHtml(indexFile, collection) {
+
+    const $ = cheerio.load(indexFile.content.toString());
+
+    console.log('processing HTML ', indexFile.name);
+
+    processScriptTags($, collection);
+    processImageTags($, collection);
+    processAnchorTags($, collection, indexFile);
+    processLinkTags($, collection);
+
+    indexFile.content = $.html();
+    await indexFile.writeFile();
+
+    console.log('done: ', indexFile.relativePath);
+}
+
+function processScriptTags($, collection) {
+    let files = collection.filterByExt('.js');
     $('script').each(function(index, el) {
         let src = $(el).attr('src');
-        if(!isRelative(src)) return;
+        if (!src) return;
+        if (!isRelative(src)) return;
         let fd = files.findOneIncludingPath(src);
 
-        if(fd) {
+        if (fd) {
             src = fd.relativePath;
         }
 
         $(el).attr('src', src);
         console.log($(el).attr('src'));
     });
+}
 
-    //ensure logo:
-    // let logo = $('img[src$="c_core.svg"]').attr('src');
-    // logo = removeInitialSlash(logo);
-    // $('img[src$="c_core.svg"]').attr('src', '/documentation/api/' + logo);
-
-    files = collection.filterByExt('jpg', 'svg', 'png');
-    // files.forEach((fd) => console.log('name', fd.basename, fd.relativePath));
-
+function processImageTags($, collection) {
+    let files = collection.filterByExt('jpg', 'svg', 'png');
     $('img').each(function(i, el) {
         let src = $(el).attr('src');
         let fd = files.findOneIncludingPath(src, false);
         console.log(src, fd.relativePath);
 
-        if(fd) {
+        if (fd) {
             src = fd.relativePath;
         }
 
         $(el).attr('src', src);
     });
+}
 
-    files = collection.filterByExt('.html');
-
+function processAnchorTags($, collection, indexFile) {
     console.log('----- Make pretty links, also ensure no relative links');
+
+    let files = collection.filterByExt('.html');
     $('a').each(function(i, el) {
         let href = $(el).attr('href');
-        if(!href) return;
+        if (!href || href === '/') return;
         /**
          * TODO: this might not be what we want! We can have an absolute
          * href but from a child directory: ./output/docs/api/index.html
          * href = /assets/logo.svg should be /docs/api/assets/logo.svg
          */
-        if(!href || !isRelative(href)) return;
+        if (!isRelative(href)) return console.log('skipping %s', href);
+
         let fd = files.findOneIncludingPath(href);
 
-        if(fd) {
+        if (fd) {
             href = fd.relativePath;
         }
 
-        links.map((rule)=> {
-            if((rule.match(href))){
+        LINK_RULES.map(rule => {
+            if (rule.match(href)) {
                 href = rule.replace(href);
             }
         });
+
+        //Include path in class.
+        // $(el).addClass(href.replace(/\//g, '-').replace(/^-/, ''));
+
+        /**
+         * If link is /documentation/api we should select both
+         * /documentation and /documentation/api
+         */
+        if (indexFile.relativePath.replace('.html', '') === href) {
+            //Select parents as well :)
+            //if we are in /documentation/about.html then select:
+            //   /documentation
+            //   /documentation/about
+            console.log('::::::::::::::::::::::::::::::');
+            console.log('href %s  %s filename ', href, indexFile.relativePath);
+            console.log('::::::::::::::::::::::::::::::');
+            $(el).addClass('selected');
+            href = '#';
+        }
 
         $(el).attr('href', href);
 
         console.log('  update', $(el).html(), href);
     });
+}
 
-    files = collection.filterByExt('.css');
-
+function processLinkTags($, collection) {
     //ensure link:
     console.log('---------- replace links ------');
+    let files = collection.filterByExt('.css');
     $('link').each(function(index, el) {
         let href = $(el).attr('href');
-        
+
         let fd = files.findOneIncludingPath(href, false);
 
-        if(fd) {
+        if (fd) {
             href = fd.relativePath;
         }
-        // href = removeInitialSlash(href);
+
         $(el).attr('href', href);
         console.log($(el).attr('href'));
     });
     console.log('---------------------------------');
-    // console.log(isRelative('#'))
-    // console.log(isRelative('/'))
-    // console.log(isRelative('/quickstart'))
-    // console.log(isRelative('http://localhost:9090/quickstart'))
-    // console.log(isRelative('quickstart/assets/peperone'))
-
-    // console.log($.html())
-    await fx.writeFile('./output/documentation/api/index2.html', $.html());
-    console.log('done!');
-}).catch(console.error);
-
-function ensureIsInPath(uri, path) {
-
 }
+    
 
-function removeInitialSlash(str='') {
+function ensureIsInPath(uri, path) {}
+
+function removeInitialSlash(str = '') {
     return str.replace(/^\//, '');
 }
 
@@ -167,14 +237,14 @@ function isAbsolute(url) {
     return /^[a-z][a-z0-9+.-]*:/.test(url);
 }
 
-function isRelative(url='') {
-    return !isAbsolute(url) && !isHash(url) && !isLocal(url);
+function isRelative(url = '') {
+    return !isAbsolute(url) && !isHash(url)// && !isLocal(url);
 }
 
-function isHash(url=''){
+function isHash(url = '') {
     return url.indexOf('#') === 0;
 }
 
-function isLocal(url='') {
+function isLocal(url = '') {
     return url.indexOf('/') === 0;
 }
